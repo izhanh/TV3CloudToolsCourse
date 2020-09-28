9e6f719435d9c9298b879dbf0da3df78018eff10
izhanh <izhan2@msn.com>
2020-09-28 09:12:44 +0200

    1st session

---
+++ 1. Preparación del entorno/.vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-proxyconf",["= 2.0.10"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.25.0"]],["vagrant-winnfsd",["= 1.4.0"]]],"checksum":"a7c42451f12cb30dad7d655701ff4d19882249fda0585984462a224c07f3b296","vagrant_version":"2.2.10"}
